e6b63792bb52caf11cf307ec6deff9d7f974a4c8
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-10-18 17:08:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from quote import getonequote
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
