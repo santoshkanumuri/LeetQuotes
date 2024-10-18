ea310e32c76e949a3f7024db7d52bffc450bc754
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-10-18 18:01:32 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -1,6 +1,8 @@
 import os
 import google.generativeai as genai
 from google.ai.generativelanguage_v1beta.types import content
+import requests
+from bs4 import BeautifulSoup as bs
 
 def connect_ai():
   GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
@@ -24,7 +26,12 @@ def connect_ai():
 
   return chat_session
 
-def getonequote(question):
+def getonequote(slug_name):
+  data = {"operationName":"questionData","variables":{"titleSlug":slug_name},"query":"query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    langToValidPlayground\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    envInfo\n    libraryUrl\n    __typename\n  }\n}\n"}
+  r = requests.post('https://leetcode.com/graphql', json = data).json()
+  soup = bs(r['data']['question']['content'], 'html.parser')
+  question= soup.text
+  print(question)
   chat_session = connect_ai()
   question = f'''Given the following problem description from LeetCode: {question}, provide two things in a JSON format:
 
